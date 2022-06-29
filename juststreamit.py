a053f05695e98e5813078326265bf4ac79f0bc86
Herve_win <rv.monnier@free.fr>
2022-06-29 17:23:45 +0200

    refactoring OK, tests W3C for the html file and the css file are Ok

--- juststreamit.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://localhost:8000/api/v1/titles/9"
-
-response = requests.get(url)
-
-print(response.json())
--- script.js
+++ script.js
@@ -1,4 +1,81 @@
+class Carousel {
+    constructor(detailMoviesObject, nameGenre, instanceNumber, moviesNumber) {
+        let idCarousel = "carousel_" + instanceNumber.toString();
+        let idImages = "images_" + instanceNumber.toString();
+        let idLeftButton = "leftButton_" + instanceNumber.toString();
+        let idRightButton = "rightButton_" + instanceNumber.toString();
 
+        this.instanceNumber = instanceNumber;
+        this.indexImage = 0;//index of the image on the left in the carousel
+        this.numberImages = moviesNumber;
+
+
+        let body = document.querySelector("body");
+
+        let newH1 = document.createElement("h1");
+        newH1.innerText = nameGenre;
+        newH1.id = "title_" + instanceNumber.toString();
+        body.appendChild(newH1);
+
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("carousel");
+        newDiv.id = idCarousel;
+        body.appendChild(newDiv);
+
+        let div = document.querySelector("#" + idCarousel)
+
+        let newButton = document.createElement("input")
+        newButton.id = idLeftButton
+        newButton.type = "button"
+        newButton.value = "<<"
+        div.appendChild(newButton);
+
+        newDiv = document.createElement("div");
+        newDiv.classList.add("images");
+        newDiv.id = idImages
+        div.appendChild(newDiv);
+
+        div = document.querySelector("#" + idImages);
+        //add the images in the carousel
+        for (let i = 0; i < moviesNumber; i++) {
+            //id = "img1_2" ==> image one of the carousel two
+            let id = "im" + i.toString() + "_" + this.instanceNumber.toString();
+            let newImg = document.createElement("img");
+            newImg.src = detailMoviesObject[id].image_url;
+            newImg.alt = detailMoviesObject[id].title;
+            newImg.id = id;
+            newImg.style.order = i.toString();
+            div.appendChild(newImg);
+        };
+
+        newButton = document.createElement("input");
+        newButton.id = idRightButton;
+        newButton.type = "button";
+        newButton.value = ">>";
+        div = document.querySelector("#" + idCarousel);
+        div.appendChild(newButton);
+
+    }
+
+    /*to scroll the images we change the css order property of the
+    leftmost image if we press the left button or the rightmost image
+    (which is not visible) if we press the right button (plus or minus the number of images)*/
+
+    rightButton() {
+        let indice = (this.indexImage + this.numberImages - 1) % this.numberImages;
+        let im = document.getElementById("im" + indice.toString() + "_" + this.instanceNumber.toString());
+        im.style.order = (parseInt(im.style.order) - this.numberImages).toString();
+        this.indexImage--;
+        if (this.indexImage == -1) { this.indexImage = this.numberImages - 1 };
+
+    }
+    leftButton() {
+        let im = document.getElementById("im" + this.indexImage.toString() + "_" + this.instanceNumber.toString());
+        im.style.order = (parseInt(im.style.order) + this.numberImages).toString();
+        this.indexImage++;
+        if (this.indexImage == this.numberImages) { this.indexImage = 0 };
+    }
+}
 async function fetchMoviesJSON(urls, counter, baseUrl) {
     let moviesLists = []
     for (let url of urls) {
@@ -29,70 +106,67 @@ async function fetchMoviesJSON(urls, counter, baseUrl) {
 }
 
 let genres = {
-    "Toutes catégories": "",
+    "Films les mieux notés": "",
     "Animation": "Animation",
     "Aventure": "Adventure",
     "Action": "Action",
 };
-let nameGenres = Object.keys(genres);
+let moviesNumber = 7;
 const baseUrl = "http://localhost:8000/api/v1/titles/";
 const sortQuery = "?sort_by=-imdb_score"
 const urls = [baseUrl + sortQuery];
+const nameGenres = Object.keys(genres);
 for (let name of nameGenres.slice(1,)) { urls.push(baseUrl + sortQuery + "&genre=" + genres[name]) };
-let moviesNumber = 7;
+
 fetchMoviesJSON(urls, moviesNumber + 1, baseUrl)
     .then(detailMoviesObject => display(detailMoviesObject, moviesNumber));
 
-
 function display(detailMoviesObject, moviesNumber) {
-    let bestMovie = detailMoviesObject["imgBestMovie"];
 
+    let bestMovie = detailMoviesObject["imgBestMovie"];
     document.querySelector("#imgBestMovie").src = bestMovie.image_url;
     document.querySelector("#imgBestMovie").alt = bestMovie.title;
     document.querySelector("#titleBestMovie").innerText = bestMovie.title;
     document.querySelector("#summaryBestMovie").innerText = bestMovie.description;
+
     let carousels = [];
     for (let i in nameGenres) {
         carousels.push(new Carousel(detailMoviesObject, nameGenres[i], i, moviesNumber));
     }
+
     const body = document.querySelector("body");
     body.onclick = function (event) {
 
         const element = event.target;
+        //click on the right or left buttons
         if (element.nodeName == "INPUT") {
             let elementSplit = element.id.split("_");
             carousels[parseInt(elementSplit[1])][elementSplit[0]]();
         }
-        else if (element.nodeName == "IMG") { displayModal(detailMoviesObject[element.id], modal) }
+        //click on an image but not the logo (the logo has not id)
+        else if (element.nodeName == "IMG" && element.id) { displayModal(detailMoviesObject[element.id], modal) }
     }
 
+    //add links for categories
     let ul = document.querySelector("ul");
     for (let i in nameGenres) {
         let newLi = document.createElement("li");
         let newA = document.createElement("a");
-        newA.href = "#carousel_" + i.toString();
+        newA.href = "#title_" + i.toString();
         newA.innerText = nameGenres[i];
         newLi.appendChild(newA)
         ul.appendChild(newLi);
     }
 
-    // Get the modal
-    var modal = document.getElementById("myModal");
+    // modal window management
+    let modal = document.getElementById("myModal");
+    let button = document.getElementById("details");
+    let span = document.getElementsByClassName("close")[0];
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("details");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the modal
-    btn.onclick = function () {
-
+    button.onclick = function () {
         displayModal(detailMoviesObject["imgBestMovie"], modal)
-
     }
 
-    // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
     }
@@ -103,9 +177,6 @@ function display(detailMoviesObject, moviesNumber) {
             modal.style.display = "none";
         }
     }
-
-
-
 }
 function displayModal(movieDetail, modal) {
     modal.style.display = "block";
@@ -133,130 +204,3 @@ function displayModal(movieDetail, modal) {
     }
 
 }
-
-
-
-
-
-class Carousel {
-    constructor(detailMoviesObject, nameGenre, instanceNumber, moviesNumber) {
-        let idCarousel = "carousel_" + instanceNumber.toString();
-        let idImages = "images_" + instanceNumber.toString();
-        let idLeftButton = "leftButton_" + instanceNumber.toString();
-        let idRightButton = "rightButton_" + instanceNumber.toString();
-        let body = document.querySelector("body");
-        let newDiv = document.createElement("div");
-
-
-        this.instanceNumber = instanceNumber;
-        this.posCarousel = 0;
-        this.numberImages = moviesNumber;
-        const lengthCarousel = 4;
-        this.maxPosCarousel = this.numberImages - lengthCarousel - 1;
-
-        let newH1 = document.createElement("h1");
-        newH1.innerText = nameGenre;
-        body.appendChild(newH1);
-
-
-        newDiv.classList.add("carousel");
-        newDiv.id = idCarousel;
-        body.appendChild(newDiv);
-
-        newDiv = document.createElement("div");
-        newDiv.classList.add("images");
-        newDiv.id = idImages
-
-        let newButton = document.createElement("input")
-        newButton.id = idLeftButton
-        newButton.type = "button"
-        newButton.value = "<<"
-
-
-
-        let div = document.querySelector("#" + idCarousel)
-        div.appendChild(newButton);
-        div.appendChild(newDiv);
-
-
-
-        div = document.querySelector("#" + idImages);
-        for (let i = 0; i < moviesNumber; i++) {
-
-            let id = "im" + i.toString() + "_" + this.instanceNumber.toString();
-
-            let newImg = document.createElement("img");
-            newImg.src = detailMoviesObject[id].image_url;
-            newImg.alt = detailMoviesObject[id].title;
-            newImg.id = id;
-            newImg.style.order = i.toString();
-            div.appendChild(newImg);
-        };
-
-        newButton = document.createElement("input");
-        newButton.id = idRightButton;
-        newButton.type = "button";
-        newButton.value = ">>";
-
-
-
-        div = document.querySelector("#" + idCarousel);
-        div.appendChild(newButton);
-
-    }
-
-    rightButton() {
-        if (this.posCarousel > 0) {
-            this.posCarousel--;
-            let im = document.getElementById("im" + this.posCarousel.toString() + "_" + this.instanceNumber.toString());
-            im.style.order = (parseInt(im.style.order) - this.numberImages).toString();
-        }
-    }
-    leftButton() {
-        if (this.posCarousel <= this.maxPosCarousel) {
-            let im = document.getElementById("im" + this.posCarousel.toString() + "_" + this.instanceNumber.toString());
-            im.style.order = (parseInt(im.style.order) + this.numberImages).toString();
-            this.posCarousel++;
-        }
-    }
-}
-
-/*
-    const imageGenres = [["https://m.media-amazon.com/images/M/MV5BNjllNTdmYzItM2IwZi00OGIzLWJmZGItZDNiNjNjOGUyMzM5XkEyXkFqcGdeQXVyOTY3MzgxNTE@._V1_UY268_CR3,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BNjNiYTIwYmUtZDlmYy00MWMxLWI0OTAtMmRkODdlMTM1ZTVmXkEyXkFqcGdeQXVyNDcwNDE0Nzk@._V1_UX182_CR0,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BODc0NjViOTUtZWM1Ny00NWQyLTlhNTgtYmYxNTAxNzRkMGEwXkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_UY268_CR14,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BOWVmOWRmMWMtMDc2OC00NGM2LTllOTAtMmY5NjVhM2YzYjVlXkEyXkFqcGdeQXVyOTc2MTgwNjY@._V1_UY268_CR3,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BMWFhZDhkYzMtYjM3NC00OTAxLThhNTEtNmM3OGFjODE0OWU3XkEyXkFqcGdeQXVyMTE5NjAxMDQx._V1_UY268_CR73,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BOTk4NGM0NmUtOTc2Yi00NTcxLWE3NGItYzEwODZjNzlhZjE2XkEyXkFqcGdeQXVyNTgyNTA4MjM@._V1_UX182_CR0,0,182,268_AL_.jpg",
-    ], ["https://m.media-amazon.com/images/M/MV5BNjllNTdmYzItM2IwZi00OGIzLWJmZGItZDNiNjNjOGUyMzM5XkEyXkFqcGdeQXVyOTY3MzgxNTE@._V1_UY268_CR3,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BNjNiYTIwYmUtZDlmYy00MWMxLWI0OTAtMmRkODdlMTM1ZTVmXkEyXkFqcGdeQXVyNDcwNDE0Nzk@._V1_UX182_CR0,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BODc0NjViOTUtZWM1Ny00NWQyLTlhNTgtYmYxNTAxNzRkMGEwXkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_UY268_CR14,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BOWVmOWRmMWMtMDc2OC00NGM2LTllOTAtMmY5NjVhM2YzYjVlXkEyXkFqcGdeQXVyOTc2MTgwNjY@._V1_UY268_CR3,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BMWFhZDhkYzMtYjM3NC00OTAxLThhNTEtNmM3OGFjODE0OWU3XkEyXkFqcGdeQXVyMTE5NjAxMDQx._V1_UY268_CR73,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BOTk4NGM0NmUtOTc2Yi00NTcxLWE3NGItYzEwODZjNzlhZjE2XkEyXkFqcGdeQXVyNTgyNTA4MjM@._V1_UX182_CR0,0,182,268_AL_.jpg",
-    ], ["https://m.media-amazon.com/images/M/MV5BNjllNTdmYzItM2IwZi00OGIzLWJmZGItZDNiNjNjOGUyMzM5XkEyXkFqcGdeQXVyOTY3MzgxNTE@._V1_UY268_CR3,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BNjNiYTIwYmUtZDlmYy00MWMxLWI0OTAtMmRkODdlMTM1ZTVmXkEyXkFqcGdeQXVyNDcwNDE0Nzk@._V1_UX182_CR0,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BODc0NjViOTUtZWM1Ny00NWQyLTlhNTgtYmYxNTAxNzRkMGEwXkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_UY268_CR14,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BOWVmOWRmMWMtMDc2OC00NGM2LTllOTAtMmY5NjVhM2YzYjVlXkEyXkFqcGdeQXVyOTc2MTgwNjY@._V1_UY268_CR3,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BMWFhZDhkYzMtYjM3NC00OTAxLThhNTEtNmM3OGFjODE0OWU3XkEyXkFqcGdeQXVyMTE5NjAxMDQx._V1_UY268_CR73,0,182,268_AL_.jpg",
-        "https://m.media-amazon.com/images/M/MV5BOTk4NGM0NmUtOTc2Yi00NTcxLWE3NGItYzEwODZjNzlhZjE2XkEyXkFqcGdeQXVyNTgyNTA4MjM@._V1_UX182_CR0,0,182,268_AL_.jpg",
-    ]]
-
-
-window.onload = function () {
-    let moviesNumber = 6;
-    let carousels = [];
-    for (images of imageGenres) {
-        carousels.push(new Carousel(images, carousels.length, moviesNumber));
-    }
-    const body = document.querySelector("body");
-    body.onclick = function (event) {
-        const element = event.target;
-        if (element.nodeName == "INPUT") {
-            let elementSplit = element.id.split("_");
-            carousels[parseInt(elementSplit[1])][elementSplit[0]]();
-        }
-
-    }
-
-}*/
